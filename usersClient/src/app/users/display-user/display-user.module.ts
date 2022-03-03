c58516f24a1db7dc2bb45faf6f7199569b25189d
haimfrenkel <hf36274@gmail.com>
2022-03-03 17:41:47 +0200

    5

--- usersClient/src/app/users/display-user/display-user.module.ts
+++ usersClient/src/app/users/display-user/display-user.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DisplayUserRoutingModule } from './display-user-routing.module';
-import { LanguagesModule } from 'src/app/languages/languages.module';
+import { PersonalCardComponent } from './personal-card/personal-card.component';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
+import { DetailsOfFamilyComponent } from './details-of-family/details-of-family.component';
+import { AddressPrintComponent } from '../display-user/address-print/address-print.component';
+import { ChildrenInformationComponent } from './children-information/children-information.component';
+import { LanguagesModule } from '../../languages/languages.module';
 
 
 @NgModule({
   declarations: [
+    PersonalCardComponent,
+    PersonalInformationComponent,
+    AddressPrintComponent,
+    ChildrenInformationComponent,
+    DetailsOfFamilyComponent,
   ],
   imports: [
     CommonModule,
-    DisplayUserRoutingModule,
     LanguagesModule,
 
   ]
